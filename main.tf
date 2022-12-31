dd8f88c2f687e17e19390bf51ccb68f60fd43c6a
victorekeleme <ekelemevictor99@gmail.com>
2022-12-31 03:55:03 -0800

    formatted

--- main.tf
+++ main.tf
@@ -1,29 +1,30 @@
 provider "aws" {
-  region = "us-east-2"
+  region = var.region
 }
 
 variable "vpc_cidr_block" {}
 variable "cidr_blocks" {
   type = list(object({
     cidr_block = string
-    name = string
+    name       = string
   }))
 }
+variable "region" {}
 variable "env_prefix" {}
 variable "avail_zone" {}
 
 
 resource "aws_vpc" "dev-vpc" {
   cidr_block = var.vpc_cidr_block
-  
+
   tags = {
     "Name" = "${var.env_prefix}-vpc"
   }
 }
 
 resource "aws_subnet" "dev-subnet-1" {
-  vpc_id = aws_vpc.dev-vpc.id
-  cidr_block = var.cidr_blocks[0].cidr_block
+  vpc_id            = aws_vpc.dev-vpc.id
+  cidr_block        = var.cidr_blocks[0].cidr_block
   availability_zone = var.avail_zone
 
   tags = {
@@ -36,13 +37,13 @@ data "aws_vpc" "existing_vpc" {
 }
 
 resource "aws_subnet" "dev-subnet-2" {
-    vpc_id = aws_vpc.dev-vpc.id
-    cidr_block = var.cidr_blocks[1].cidr_block
-    availability_zone = var.avail_zone
+  vpc_id            = aws_vpc.dev-vpc.id
+  cidr_block        = var.cidr_blocks[1].cidr_block
+  availability_zone = var.avail_zone
 
-    tags = {
-      "Name" = "${var.env_prefix}-${var.cidr_blocks[1].name}"
-    }
+  tags = {
+    "Name" = "${var.env_prefix}-${var.cidr_blocks[1].name}"
+  }
 }
 
 output "dev-vpc-id" {
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,8 +1,9 @@
 vpc_cidr_block = "192.168.0.0/16"
 cidr_blocks = [
-    {cidr_block="192.168.16.0/20", name="subnet-1"},
-    {cidr_block="192.168.80.0/20", name="subnet-2"}
-    ]
+  { cidr_block = "192.168.16.0/20", name = "subnet-1" },
+  { cidr_block = "192.168.80.0/20", name = "subnet-2" }
+]
 env_prefix = "dev"
 avail_zone = "us-east-2a"
+region     = "us-east-2"
 
