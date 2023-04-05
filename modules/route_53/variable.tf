9879cd03173fd43127d3d1ab5f0d608d1b590a87
victorekeleme <ekelemevictor99@gmail.com>
2023-04-05 16:12:09 -0400

    updated

---
+++ modules/route_53/variable.tf
@@ -0,0 +1,11 @@
+variable "domain_name" {
+    default = "mystudyhub.ca"
+    description = "Domain name"
+    type = string
+}
+
+variable "record_name" {
+    default = "www"
+    description = "Sub domain"
+    type = string
+}
